a8dc96058eac309b49262496a25a773a0e74f327
Thomas <1071931588@qq.com>
2019-10-28 18:05:48 +0800

    README

--- lib_skeletonblock/src/main/java/com/gykj/thomas/widget/SkeletonBlock.java
+++ lib_skeletonblock/src/main/java/com/gykj/thomas/widget/SkeletonBlock.java
@@ -1,12 +1,15 @@
-package com.gykj.thomas.simple;
+package com.gykj.thomas.widget;
 
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+
 
 import java.util.Random;
 
@@ -19,7 +22,7 @@ import java.util.Random;
  * Description:
  */
 public class SkeletonBlock extends View {
-    private Animation mAnimation;
+    private static final String TAG = "SkeletonBlock";
     public static int duration = 500;
     public SkeletonBlock(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -27,24 +30,30 @@ public class SkeletonBlock extends View {
 
     public SkeletonBlock(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SkeletonBlock);
-        final int d=typedArray.getInteger(R.styleable.SkeletonBlock_sb_duration,0);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, com.gykj.thomas.widget.R.styleable.SkeletonBlock);
+        final int d=typedArray.getInteger(com.gykj.thomas.widget.R.styleable.SkeletonBlock_sb_duration,0);
         typedArray.recycle();
+        final boolean random =new Random().nextBoolean();
         post(new Runnable() {
             @Override
             public void run() {
+
                 float v = new Random().nextFloat();
                 if(v<0.3)v=0.3f;
                 else if(v>0.8)v=0.8f;
-                mAnimation = new ScaleAnimation(1, v, 1, 1,
+
+                Animation animation;
+                animation = new ScaleAnimation(random?1:v,random?v:1, 1, 1,
                         Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0);
-                mAnimation.setDuration(d==0?duration:d);
-                mAnimation.setRepeatMode(Animation.REVERSE);
-                mAnimation.setRepeatCount(Animation.INFINITE);
-                startAnimation(mAnimation);
+                animation.setDuration(d==0?duration:d);
+                animation.setRepeatMode(Animation.REVERSE);
+                animation.setRepeatCount(Animation.INFINITE);
+
+
+                startAnimation(animation);
             }
         });
 
-
     }
+
 }
